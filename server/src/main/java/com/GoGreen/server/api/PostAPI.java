5a5fc466ea83985c4a66901fb8787572ce4eec04
AlexNguyenJJ <atn012@ucsd.edu>
2021-02-20 15:29:00 -0800

    Added and clarified two methods in PostAPI.java

--- server/src/main/java/com/GoGreen/server/api/PostAPI.java
+++ server/src/main/java/com/GoGreen/server/api/PostAPI.java
@@ -1,22 +1,37 @@
 package com.GoGreen.server.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.GoGreen.server.service.PostService;
+import com.GoGreen.server.model.Post;
 
 /**
  * API acts as messenger between front-end and back-end
  */
 @RestController
-@GetMapping("/postAPI")
+@RequestMapping("/postAPI")
 public class PostAPI
 {
 
-  private postService service;
+  private PostService service;
   /**
    * Retrieve location variable of a post
    * @return String - the location of the post
    */
+  @GetMapping(path = "{id}")
   public String getLocation(int id) {
-    /* TODO */
     return service.selectPost(id).getLocation();
   }
+
+  /**
+   * Add post to 
+   * @param post
+   * @return
+   */
+  @PostMapping
+  public boolean addPost(@RequestBody Post post) {
+    service.addPost(post);
+    return true;
+  }
+
+  private 
 }
