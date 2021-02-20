6a10c7e9e1c2d241ddef0d7b25cd909becaddcb8
AlexNguyenJJ <atn012@ucsd.edu>
2021-02-20 15:11:26 -0800

    test

--- server/src/main/java/com/GoGreen/server/api/PostAPI.java
+++ server/src/main/java/com/GoGreen/server/api/PostAPI.java
@@ -2,8 +2,21 @@ package com.GoGreen.server.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * API acts as messenger between front-end and back-end
+ */
 @RestController
+@GetMapping("/postAPI")
 public class PostAPI
 {
 
+  private postService service;
+  /**
+   * Retrieve location variable of a post
+   * @return String - the location of the post
+   */
+  public String getLocation(int id) {
+    /* TODO */
+    return service.selectPost(id).getLocation();
+  }
 }
