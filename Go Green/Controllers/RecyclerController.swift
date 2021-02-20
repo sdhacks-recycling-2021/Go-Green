2f8856ee498445d2f79307e5d1b6515ed649c073
alanpcao@gmail.com <alanpcao@gmail.com>
2021-02-20 15:35:19 -0800

    Added map view and llow app to request and display user location

--- Go Green/Controllers/RecyclerController.swift
+++ Go Green/Controllers/RecyclerController.swift
@@ -5,17 +5,48 @@
 //  Created by Alan Cao on 2/20/21.
 //
 
+import MapKit
 import UIKit
 
-class RecyclerController: UIViewController {
+class RecyclerController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var mapView: MKMapView!
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        enableLocationServices()
         configureUI()
+        
     }
 
     func configureUI() {
         navigationController?.navigationBar.isHidden = true
+        
+        configureMapView()
+    }
+    
+    func configureMapView() {
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+    }
+    
+    func enableLocationServices() {
+        switch CLLocationManager.authorizationStatus(){
+        case .notDetermined: locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied: break
+        case .authorizedAlways:
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        case .authorizedWhenInUse: locationManager.requestAlwaysAuthorization()
+        @unknown default: break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
     }
 }
--- Go Green/Controllers/SignInController.swift
+++ Go Green/Controllers/SignInController.swift
@@ -14,6 +14,7 @@ class SignInController: UIViewController {
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var signInButton: UIButton!
+    @IBOutlet var logoLabel: UILabel!
     
     // MARK: - Lifecycle
 
@@ -25,6 +26,7 @@ class SignInController: UIViewController {
     
     func configureUI() {
         navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = false
         
         emailTextField.widthAnchor.constraint(equalToConstant: view.frame.width - 80).isActive = true
         emailTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
@@ -36,6 +38,24 @@ class SignInController: UIViewController {
         signInButton.widthAnchor.constraint(equalToConstant: view.frame.width - 80).isActive = true
         signInButton.heightAnchor.constraint(equalToConstant: 48).isActive = true
         signInButton.layer.cornerRadius = 48 / 2
+    }
+    
+    @IBAction func handleSCToggled(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            view.backgroundColor = .white
+            logoLabel.textColor = .systemGreen
+            signInButton.setTitleColor(.white, for: .normal)
+            signInButton.backgroundColor = .systemGreen
+            navigationController?.navigationBar.barTintColor = .white
+        case 1:
+            view.backgroundColor = .systemGreen
+            logoLabel.textColor = .white
+            signInButton.setTitleColor(.systemGreen, for: .normal)
+            signInButton.backgroundColor = .white
+            navigationController?.navigationBar.barTintColor = .systemGreen
+        default: print("DEBUG: handleSCToggled default case")
+        }
     }
     
     @IBAction func handleSignInTapped(_ sender: Any) {
