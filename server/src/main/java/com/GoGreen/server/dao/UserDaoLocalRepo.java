522dd21dbc09d8c93c4d9fe2e85d28532bb2396a
AlexNguyenJJ <atn012@ucsd.edu>
2021-02-20 18:11:45 -0800

    Added local repository for UserDao

---
+++ server/src/main/java/com/GoGreen/server/dao/UserDaoLocalRepo.java
@@ -0,0 +1,12 @@
+package com.GoGreen.server.dao;
+
+import com.GoGreen.server.model.Post;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository("localUserDao")
+public class UserDaoLocalRepo {
+  
+}
